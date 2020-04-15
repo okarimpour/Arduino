967ea79b34dd115f2d821ce38d96d613ac11aa19
Omid Karimpour <okarimpour@gmail.com>
2020-04-15 15:43:58 -0400

    Create a workspace, package, build it, and etc

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/omid/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/omid/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/omid/catkin_ws/devel;/opt/ros/melodic' != '' else []
